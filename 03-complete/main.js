76b8740ccfa4b7eebc33634249cecd1ac6c00177
Wray Bowling <wray@richlandlibrary.com>
2017-02-10 10:41:16 -0500

    draw voronoi lines

--- 03-complete/main.js
+++ 03-complete/main.js
@@ -5,35 +5,61 @@ console.log('controls',controls);
 const BOUNDS = {xl: 0, xr: 800, yt: 0, yb: 600};
 
 var pieces = [];
+var lines = [];
 
 function draw(){
 
-
-
-
-  var d=0;
+  let d=0;
   for(let y=BOUNDS.yt+controls.bezel; y<BOUNDS.yb-controls.bezel; y+=controls.pieceSize){
     for(let x=BOUNDS.xl+controls.bezel; x<BOUNDS.xr-controls.bezel; x+=controls.pieceSize){
-      let piece = new Vec2(x,y);
-
-      pieces.push(piece);
-
-      var dot;
       if (pieces.length <= d) {
-        dot = document.createElementNS(svgNS,'circle');
-        dotGroup.appendChild(dot);
-        console.log('dot',d,dot);
-      }// else {
+        let piece = new Vec2(x,y);
+        piece.el = document.createElementNS(svgNS,'circle');
+        dotGroup.appendChild(piece.el);
+        pieces.push(piece);
+      }
+
+        pieces[d].el.setAttributeNS(null,'cx',x);
+        pieces[d].el.setAttributeNS(null,'cy',y);
+        pieces[d].el.setAttributeNS(null,'r',5);
+
+      // else {
       //   dot = dotGroup.children[i];
       // }
 
-
-
-      // dot.setAttributeNS(null,'cx',x);
-      // dot.setAttributeNS(null,'cy',y);
-      // dot.setAttributeNS(null,'r',5);
       d++;
     }
+  }
+
+  // remove old pieces
+  for(let i=0; i<(pieces.length - d); i++){
+    pieces[pieces.length-1].el.remove();
+    pieces.pop();
+  }
+
+  // build piece scaffolding
+  let v = new Voronoi();
+  let vData = v.compute(pieces,BOUNDS);
+  let l=0;
+  for(; l<vData.edges.length; l++){
+    let p1 = vData.edges[l].va;
+    let p2 = vData.edges[l].vb;
+
+    if(l >= lines.length) {
+      let line = document.createElementNS(svgNS,'line');
+      lineGroup.appendChild(line);
+      lines.push(line);
+    }
+    lines[l].setAttributeNS(null,'x1',p1.x);
+    lines[l].setAttributeNS(null,'y1',p1.y);
+    lines[l].setAttributeNS(null,'x2',p2.x);
+    lines[l].setAttributeNS(null,'y2',p2.y);
+  }
+
+  // remove old lines
+  for(let i=0; i<(lines.length - l); i++){
+    lines[lines.length-1].remove();
+    lines.pop();
   }
 
 }
