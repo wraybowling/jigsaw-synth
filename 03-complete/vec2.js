80a4cf40e473fc7453222a47e8780236c68dc595
Wray Bowling <wray@richlandlibrary.com>
2017-02-10 14:48:34 -0500

    noise seed ranges from zero to Pi

--- 03-complete/vec2.js
+++ 03-complete/vec2.js
@@ -13,9 +13,9 @@ class Vec2 {
            * Math.sin(this.originY / 0.3 / controls.pieceSize + controls.seed)
            * controls.voronoise * controls.pieceSize
            + this.originX;
-    this.y = Math.sin(this.originX / 0.01 / controls.pieceSize + controls.seed)
+    this.y = Math.sin(this.originX / 0.02 / controls.pieceSize + controls.seed)
            * Math.sin(this.originX / 0.5 / controls.pieceSize + controls.seed)
-           * Math.sin(this.originY / 0.04 / controls.pieceSize + controls.seed)
+           * Math.sin(this.originY / 0.045 / controls.pieceSize + controls.seed)
            * Math.sin(this.originY / 0.6 / controls.pieceSize + controls.seed)
            * controls.voronoise * controls.pieceSize
            + this.originY;
